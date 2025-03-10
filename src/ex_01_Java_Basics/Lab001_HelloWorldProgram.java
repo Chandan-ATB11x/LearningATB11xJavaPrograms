f84bf24f9f2f5e616ba6817cf7cc8c3c6d32163d
Chandan <chandanchandhu1108@gmail.com>
2025-03-10 09:11:50 +0530

    10-03 Pushing code

--- src/ex_01_Java_Basics/Lab001_HelloWorldProgram.java
+++ src/ex_01_Java_Basics/Lab001_HelloWorldProgram.java
@@ -3,5 +3,6 @@ package ex_01_Java_Basics;
 public class Lab001_HelloWorldProgram {
     public static void main(String[] args) {
         System.out.println("Hello world, Good Morning");
+        //write a
     }
 }
---
+++ src/ex_01_Java_Basics/Test1.java
@@ -0,0 +1,7 @@
+package ex_01_Java_Basics;
+
+public class Test1 {
+    public static void main(String[] args) {
+        System.out.println("Change Author name");
+    }
+}
